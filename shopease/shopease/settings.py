a98a5255de8a1b9030505d10d32cd991344da299
Hardik Makwana <hardik19998@gmail.com>
2021-06-15 11:36:54 +0530

    Configured for deployement

--- shopease/shopease/settings.py
+++ shopease/shopease/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-=#6dxtk7h_=wj!)h91494r34cpi=n=!yircx8f*dwdcwrx9ui'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
