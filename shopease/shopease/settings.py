aad5419268a50d4f5d4a0d891c2e56de87c6ea28
Hardik Makwana <hardik19998@gmail.com>
2021-06-15 16:16:23 +0530

    updated allowed host

--- shopease/shopease/settings.py
+++ shopease/shopease/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '-=#6dxtk7h_=wj!)h91494r34cpi=n=!yircx8f*dwdcwrx9ui'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-18-169-19-77.eu-west-2.compute.amazonaws.com',
+                '127.0.0.1']
 
 
 # Application definition
