e5692e83c9859a255298529f0e82d3d1fe2b0c5c
Abhishe-k <mehta.abhishek98@yahoo.in>
2021-06-18 01:35:18 +0530

    added edit, create and delete apis for store

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from django.conf.urls import url 
 from core import views
-from .views import RegisterView, getUser, getStore , getOwnerByEmail
+from .views import RegisterView, getUser , getOwnerByEmail
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     url(r'getUser/', views.getUser),
-    url(r'getStore/', views.getStore),
-    url(r'getOwnerByEmail/', views.getOwnerByEmail),
+        url(r'getOwnerByEmail/', views.getOwnerByEmail),
     
 ]
