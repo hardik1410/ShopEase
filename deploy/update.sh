475ba8b3119f588df12f851137112b8db4666470
Hardik Makwana <hardik19998@gmail.com>
2021-06-24 14:45:54 +0530

    modified update.sh for deployment

--- deploy/update.sh
+++ deploy/update.sh
@@ -4,7 +4,7 @@ set -e
 
 PROJECT_BASE_PATH='/usr/local/apps/ShopEase'
 
-git pull
+git pull origin Development
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 supervisorctl restart shopease
