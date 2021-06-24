0f1d3c78a11ee286a85cdc7ec5de26ba03d8e251
Hardik Makwana <hardik19998@gmail.com>
2021-06-24 15:22:24 +0530

    updated urls in core app

--- core/urls.py
+++ core/urls.py
@@ -12,8 +12,8 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('logout/', LogoutAPIView.as_view(), name="logout"),
     path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
