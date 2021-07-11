5671e251b8add84047021631c800384a8d6007c4
Hardik Makwana <hardik19998@gmail.com>
2021-07-11 17:01:43 +0530

    Changed requirements for deployment

--- deploy/update.sh
+++ deploy/update.sh
@@ -4,6 +4,8 @@ set -e
 
 PROJECT_BASE_PATH='/usr/local/apps/ShopEase'
 
+/env/bin/pip install -r requirements.txt
+
 git pull origin Development
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
